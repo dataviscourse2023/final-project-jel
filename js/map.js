1230e45968c21b47f3f8dc853b3b90c499efe3ee
Jeremy Reynolds <u0551837@utah.edu>
2023-10-29 23:15:44 -0600

    Implement map.js and script.js

---
+++ js/map.js
@@ -0,0 +1,110 @@
+/** Class representing the map view.
+ * Adapted from Visualization for Data Science Homework 4
+ */
+class MapVis {
+    /**
+     * Creates a Map Visuzation
+     * @param globalApplicationState The shared global application state (has the data and the line chart instance in it)
+     */
+    constructor(globalApplicationState) {
+        this.globalApplicationState = globalApplicationState;
+
+        // Set up the map projection
+        const projection = d3.geoWinkel3()
+            .scale(150) // This set the size of the map
+            .translate([400, 250]); // This moves the map to the center of the SVG
+
+        const path = d3.geoPath()
+            .projection(projection);
+
+        this.renderGraticules(path);
+        this.renderMap(path);
+    }
+
+    renderMap(path) {
+        const geoJSON = topojson.feature(this.globalApplicationState.mapData, this.globalApplicationState.mapData.objects.countries);
+        const maxValues = d3.rollup(
+            this.globalApplicationState.Data,
+            v => d3.max(v, d => +d.value),
+            d => d.country_code
+        );
+        const overallValues = d3.max(Array.from(maxValues.values()));
+        const colorScale = d3.scaleSequential(d3.interpolateReds)
+            .domain([0, overallValues]);
+
+        this.countries = d3.select('#map')
+            .select('#countries');
+
+        this.countries.selectAll('path')
+            .data(geoJSON.features)
+            .enter().append('path')
+            .attr('d', path)
+            .attr('class', 'country')
+            .attr('id', d => d.id)
+            .on('mouseover', (event, d) => this.mouseOver(event, d))
+            .on('mouseleave', (event, d) => this.mouseLeave(event, d))
+            .on('click', (event, _d) => this.displayModal(event));
+
+        this.countries.selectAll('.country')
+            .style('fill', d => {
+                const countrymaxValues = maxValues.get(d.id);
+                if (countrymaxValues !== undefined) {
+                    return colorScale(countrymaxValues);
+                } else {
+                    return '#ccc';
+                }
+            });
+    }
+
+    renderGraticules(path) {
+        const graticules = d3.select('#map')
+            .select('#graticules');
+        const grat = d3.geoGraticule();
+
+        graticules.selectAll('.graticule-line')
+            .data(grat.lines())
+            .enter().append('path')
+            .attr('class', 'graticule-line')
+            .attr('d', path)
+
+        graticules.append('path')
+            .datum(grat.outline())
+            .attr('class', 'graticule outline')
+            .attr('d', path)
+    }
+
+    // Adapted from https://d3-graph-gallery.com/graph/choropleth_hover_effect.html
+    mouseOver(event, _d) {
+        d3.selectAll('.country')
+            .transition()
+            .duration(0)
+            .style('opacity', 0.75);
+
+        // Highlight country under mouse
+        d3.select(event.currentTarget)
+            .transition()
+            .duration(0)
+            .style('opacity', 1);
+        // .style("stroke", "black");        
+    }
+
+    // Adapted from https://d3-graph-gallery.com/graph/choropleth_hover_effect.html
+    mouseLeave(_event, _d) {
+        d3.selectAll('.country')
+            .transition()
+            .duration(0)
+            .style('opacity', 1);
+        // .style('opacity', 1)
+        // .style("stroke", "none");
+    }
+
+    displayModal(event) {
+        // TODO: Implement error handling for when countryName is undefined
+        const countryName = this.globalApplicationState.Data.filter(d => d.country_code === event.currentTarget.id)[0].country_name;
+        
+        // if (countryName != undefined) {
+            d3.select('#country_name').text(countryName);
+            modal.style.display = 'block';
+        // }
+    }
+}
--- js/script.js
+++ js/script.js
@@ -1,15 +1,92 @@
 // world.json source: Visualization for Data Science Homework 4
+// Adapted from Visualization for Data Science Homework 4
 
-setup();
-
-function setup() {
-    changeData();
+// ******* DATA LOADING *******
+async function loadData() {
+    const co2EmissionsData = await d3.csv('data/co2_emissions.csv');
+    const methaneEmissionsData = await d3.csv('data/methane_emissions.csv');
+    const deforestationData = await d3.csv('data/net_forest.csv');
+    const temperatureData = await d3.csv('data/surface_tempurature.csv');
+    const dataArrays = [co2EmissionsData, methaneEmissionsData, deforestationData, temperatureData];
+    const mapData = await d3.json('data/world.json');
+    return { dataArrays, mapData };
 }
 
-function update(data) {
+// ******* STATE MANAGEMENT *******
+const globalApplicationState = {
+    selectedLocations: [],
+    Data: null,
+    mapData: null,
+    worldMap: null,
+    // lineChart: null,
+};
 
-}
+//******* APPLICATION MOUNTING *******
+loadData().then((loadedData) => {
+    console.log('Here is the imported data:', loadedData.dataArrays[0]);
 
-function changeData() {
+    // Store the loaded data into the globalApplicationState
+    globalApplicationState.Data = loadedData.dataArrays[0];
+    globalApplicationState.mapData = loadedData.mapData;
 
-}
+    // Creates the view objects with the global state passed in 
+    const worldMap = new MapVis(globalApplicationState);
+    // const lineChart = new LineChart(globalApplicationState);
+
+    globalApplicationState.worldMap = worldMap;
+    // globalApplicationState.lineChart = lineChart;
+
+    d3.select('#co2Btn')
+        .on('click', () => {
+            globalApplicationState.Data = null;
+            globalApplicationState.Data = loadedData.dataArrays[0];
+            worldMap.renderMap();
+        });
+
+        d3.select('#methaneBtn')
+        .on('click', () => {
+            globalApplicationState.Data = null;
+            globalApplicationState.Data = loadedData.dataArrays[1];
+            worldMap.renderMap();
+        });
+
+        d3.select('#deforestationBtn')
+        .on('click', () => {
+            globalApplicationState.Data = null;
+            globalApplicationState.Data = loadedData.dataArrays[2];
+            worldMap.renderMap();
+        });
+
+        d3.select('#temperatureBtn')
+        .on('click', () => {
+            globalApplicationState.Data = null;
+            globalApplicationState.Data = loadedData.dataArrays[3];
+            worldMap.renderMap();
+        });
+});
+
+// Get modal element
+const modal = document.getElementById('countryModal');
+
+// Get the button that opens the modal
+const openModalBtn = document.getElementById('openModalBtn');
+
+// Get the element that closes the modal
+const closeModalBtn = document.getElementById('closeModalBtn');
+
+// Open the modal
+openModalBtn.addEventListener('click', () => {
+    modal.style.display = 'block';
+});
+
+// Close the modal
+closeModalBtn.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
+
+// Close the modal if the user clicks outside of it
+window.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.style.display = 'none';
+    }
+});
