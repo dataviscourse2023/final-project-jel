189001fe60c1f01dfa7336785ccf02a837157b4e
Ellery Gresham <u1276249@utah.edu>
2023-11-01 14:38:14 -0600

    Added to Introduction section with info

---
+++ js/bar-chart.js
@@ -0,0 +1,15 @@
+/** Class representing the bar chart visualizations.
+ */
+
+class BarChartVis{
+
+    /**
+     * Creates a Bar Chart Visualzation 
+     * @param globalApplicationState 
+     */
+    constructor(globalApplicationState) {
+
+        this.globalApplicationState = globalApplicationState;
+    }
+    
+}
