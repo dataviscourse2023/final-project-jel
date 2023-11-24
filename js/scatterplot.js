46f25fa33fac65ced50854908263a56de2007e8d
Saint-Luke <103982254+Saint-Luke@users.noreply.github.com>
2023-11-24 15:25:38 -0700

    Added background image.

---
+++ js/scatterplot.js
@@ -0,0 +1,15 @@
+/** Class representing the scatterplot visualizations.
+ */
+
+class ScatterplotVis {
+
+    /**
+     * Creates a Bar Chart Visualzation 
+     * @param globalApplicationState 
+     */
+    constructor(globalApplicationState) {
+
+        this.globalApplicationState = globalApplicationState;
+    }
+
+}
